0f38407872ed0ad469422c6f6700fa28ee9f3605
Agatha <agathafranca@outlook.com>
2021-06-11 18:47:41 -0300

    Adicionar arquivos de projeto.

---
+++ StudentBank.InternalSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using StudentBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentBank.InternalSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(458, 455789);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
